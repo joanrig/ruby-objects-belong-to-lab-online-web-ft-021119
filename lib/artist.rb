284509e3cfe237723e55d4fce0e95bc7352a60b3
Joan Lyness <joanrigdon@gmail.com>
2019-02-25 05:26:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,10 @@
 class Artist
-  attr_accessor :name :genre
+  attr_accessor :name, :genre
 
   def initialize(name, genre)
     @name = name
     @genre = genre
   end
 
-  @name = Artist.new.name
-  @genre = Artist.new.genre
 
 end
